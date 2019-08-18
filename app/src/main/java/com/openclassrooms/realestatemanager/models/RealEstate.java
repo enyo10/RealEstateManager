64e65ac92a8fd049b573b1257dd3e644ffec1c07
enyo10 <enyo10@yahoo.fr>
2019-08-18 21:35:49 +0200

    StorageUtils class created

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/models/RealEstate.java
@@ -0,0 +1,21 @@
+package com.openclassrooms.realestatemanager.models;
+
+import java.util.Date;
+
+public class RealEstate {
+
+    private int mPrice;
+    private double mArea;
+    private int mNumberOfPiece;
+    private String mCompleteDescription;
+    private String mPhotoUrl;
+    private Address mAddress;
+    private Status mStatus;
+    private Date mDateOfEntry;
+    private Date mDateOfSale;
+    private Administrator mAdministrator;
+
+
+
+
+}
